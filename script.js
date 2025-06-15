938b74e1e3d30f36f24a12ca7943a88591cb996d
Parth Rupesh Farande <parthfarande@gmail.com>
2025-06-15 16:06:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
+
 function addRecommendation() {
-    // Get the message of the new recommendation
+  const recommendation = document.getElementById("new_recommendation");
+  if (recommendation.value.trim() !== "") {
+    const element = document.createElement("div");
+    element.className = "project";
+    element.innerHTML = `<p>\"${recommendation.value.trim()}\"</p>`;
+    document.getElementById("all_recommendations").appendChild(element);
+    recommendation.value = "";
     showPopup(true);
-    let recommendation = document.getElementById("new_recommendation");
-    // If the user has left a recommendation, display a pop-up
-    if (recommendation.value != null && recommendation.value.trim() != "") {
-      console.log("New recommendation added");
-      //Call showPopup here
-      showPopup("New Recommendation added");
-      // Create a new 'recommendation' element and set it's value to the user's message
-      var element = document.createElement("div");
-      element.setAttribute("class","recommendation");
-      element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-      // Add this element to the end of the list of recommendations
-      document.getElementById("all_recommendations").appendChild(element); 
-      
-      // Reset the value of the textarea
-      recommendation.value = "";
-    }
   }
-  
-  function showPopup(bool) {
-    if (bool) {
-      document.getElementById('popup').style.visibility = 'visible'
-    } else {
-      document.getElementById('popup').style.visibility = 'hidden'
-    }
+}
+function showPopup(bool) {
+  const popup = document.getElementById("popup");
+  if (bool) {
+    popup.classList.remove("hidden");
+    popup.classList.add("visible");
+  } else {
+    popup.classList.remove("visible");
+    popup.classList.add("hidden");
   }
-  
+}
